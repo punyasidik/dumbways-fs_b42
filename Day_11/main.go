8cc03e2a600f9babebbc60797fb6188cdd199893
Ahmad Sidik Rudini <sidikrudini16@gmail.com>
2022-12-03 18:09:45 +0700

    day 11

---
+++ Day_11/main.go
@@ -0,0 +1,394 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"personal-web/connection"
+	"strconv"
+	"strings"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
+)
+
+func main() {
+	route := mux.NewRouter()
+
+	connection.DatabaseConnect()
+
+	//route untuk menginisialisai folder public
+	route.PathPrefix("/public/").Handler(http.StripPrefix("/public/", http.FileServer(http.Dir("./public"))))
+
+	route.HandleFunc("/", home).Methods("GET")
+	// route.HandleFunc("/formblog", formBlog).Methods("GET")
+	route.HandleFunc("/blog-detail/{id}", blogDetail).Methods("GET")
+	route.HandleFunc("/blog", blog).Methods("GET")
+	route.HandleFunc("/contact", contact).Methods("GET")
+	route.HandleFunc("/add-blog", addBlog).Methods("POST")
+	route.HandleFunc("/add-blog", formAddBlog).Methods("GET")
+	route.HandleFunc("/delete-blog/{id}", deleteBlog).Methods("GET")
+
+	route.HandleFunc("/register", formRegister).Methods("GET")
+	route.HandleFunc("/register", register).Methods("POST")
+
+	route.HandleFunc("/login", formLogin).Methods("GET")
+	route.HandleFunc("/login", login).Methods("POST")
+
+	route.HandleFunc("/logout", logout).Methods("GET")
+
+	fmt.Println("Server berjalan pada port 5000")
+	http.ListenAndServe("localhost:5000", route)
+}
+
+type MetaData struct {
+	Title     string
+	IsLogin   bool
+	UserName  string
+	FlashData string
+}
+
+var Data = MetaData{
+	Title: "Personal Web",
+}
+
+type Blog struct {
+	ID          int
+	Title       string
+	Content     string
+	Image       string
+	Post_date   time.Time
+	Format_date string
+	Author      string
+}
+
+type User struct {
+	Id       int
+	Name     string
+	Email    string
+	Password string
+}
+
+// var blogs = []Blog{}
+
+// var blogs = []Blog{
+// 	{
+// 		Title:   "Samsul Rijal",
+// 		Content: "Hallo Dumbways",
+// 		// Post_date: "24 November 2022",
+// 		Author: "Samsul Rijal",
+// 	},
+// }
+
+func addBlog(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	title := r.PostForm.Get("title")
+	content := r.PostForm.Get("content")
+
+	// var newBlog = Blog{
+	// 	Title:   title,
+	// 	Content: content,
+	// 	// Post_date: "24 November 2022",
+	// 	Author: "Samsul Rijal",
+	// }
+
+	_, err = connection.Conn.Exec(context.Background(), "INSERT INTO tb_blog(title, content, images) VALUES ($1, $2, 'images.png')", title, content)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("message : " + err.Error()))
+		return
+	}
+
+	// blogs.push(newBlog)
+	// blogs = append(blogs, newBlog)
+
+	http.Redirect(w, r, "/blog", http.StatusMovedPermanently)
+}
+
+func blog(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/html; charset=utf-8")
+	tmpt, err := template.ParseFiles("views/blog.html")
+
+	// if condition
+	if err != nil {
+		w.Write([]byte("Message : " + err.Error()))
+		return
+	}
+
+	dataBlog, errQuery := connection.Conn.Query(context.Background(), "SELECT id, title, content, post_date FROM tb_blog")
+	if errQuery != nil {
+		fmt.Println("Message : " + errQuery.Error())
+		return
+	}
+
+	var result []Blog
+
+	for dataBlog.Next() {
+		var each = Blog{}
+
+		err := dataBlog.Scan(&each.ID, &each.Title, &each.Content, &each.Post_date)
+		if err != nil {
+			fmt.Println("Message : " + err.Error())
+			return
+		}
+
+		each.Author = "Abel Dustin"
+		each.Format_date = each.Post_date.Format("2 January 2006")
+
+		result = append(result, each)
+	}
+
+	fmt.Println(result)
+	resData := map[string]interface{}{
+		"Blogs": result,
+	}
+
+	tmpt.Execute(w, resData)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/html; charset=utf-8")
+	tmpt, err := template.ParseFiles("views/index.html")
+
+	if err != nil {
+		w.Write([]byte("Message : " + err.Error()))
+		return
+	}
+
+	var store = sessions.NewCookieStore([]byte("SESSIONS_ID"))
+	session, _ := store.Get(r, "SESSIONS_ID")
+
+	if session.Values["IsLogin"] != true {
+		Data.IsLogin = false
+	} else {
+		Data.IsLogin = session.Values["IsLogin"].(bool)
+		Data.UserName = session.Values["Name"].(string)
+	}
+
+	fm := session.Flashes("message")
+
+	var flashes []string
+	if len(fm) > 0 {
+		session.Save(r, w)
+
+		for _, fl := range fm {
+			flashes = append(flashes, fl.(string))
+		}
+	}
+
+	Data.FlashData = strings.Join(flashes, "")
+
+	tmpt.Execute(w, Data)
+}
+
+func formAddBlog(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/html; charset=utf-8")
+	tmpt, err := template.ParseFiles("views/add-blog.html")
+
+	if err != nil {
+		w.Write([]byte("Message : " + err.Error()))
+		return
+	}
+
+	tmpt.Execute(w, nil)
+}
+
+func blogDetail(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/html; charset=utf-8")
+	tmpt, err := template.ParseFiles("views/blog-detail.html")
+
+	if err != nil {
+		w.Write([]byte("Message : " + err.Error()))
+		return
+	}
+
+	id, _ := strconv.Atoi(mux.Vars(r)["id"])
+
+	// object golang
+	// data := map[string]interface{}{
+	// 	"Title":   "Pasar Coding Dari Dumbways",
+	// 	"Content": "REPUBLIKA.CO.ID, JAKARTA -- Ketimpangan sumber daya manusia (SDM) di sektor digital masih menjadi isu yang belum terpecahkan.",
+	// 	"Id":      id,
+	// }
+	var BlogDetail = Blog{}
+
+	// for index, data := range blogs {
+	// 	if index == id {
+	// 		BlogDetail = Blog{
+	// 			Title:     data.Title,
+	// 			Content:   data.Content,
+	// 			Post_date: data.Post_date,
+	// 			Author:    data.Author,
+	// 		}
+	// 	}
+	// }
+
+	// add code
+	err = connection.Conn.QueryRow(context.Background(), "SELECT id, title, image, content, post_date FROM tb_blog WHERE id=$1", id).Scan(
+		&BlogDetail.ID, &BlogDetail.Title, &BlogDetail.Image, &BlogDetail.Content, &BlogDetail.Post_date,
+	)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("message : " + err.Error()))
+		return
+	}
+
+	BlogDetail.Author = "Abel Dustin"
+	BlogDetail.Format_date = BlogDetail.Post_date.Format("2 January 2006")
+
+	fmt.Println(BlogDetail)
+
+	dataDetail := map[string]interface{}{
+		"Blog": BlogDetail,
+	}
+
+	tmpt.Execute(w, dataDetail)
+}
+
+func deleteBlog(w http.ResponseWriter, r *http.Request) {
+
+	id, _ := strconv.Atoi(mux.Vars(r)["id"])
+	// fmt.Println(index)
+
+	// blogs = append(blogs[:index], blogs[index+1:]...)
+	_, err := connection.Conn.Exec(context.Background(), "DELETE FROM tb_blog WHERE id=$1", id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("message : " + err.Error()))
+		return
+	}
+
+	http.Redirect(w, r, "/blog", http.StatusMovedPermanently)
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/html; charset=utf-8")
+	tmpt, err := template.ParseFiles("views/contact.html")
+
+	if err != nil {
+		w.Write([]byte("Message : " + err.Error()))
+		return
+	}
+
+	tmpt.Execute(w, nil)
+}
+
+func formRegister(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	var tmpl, err = template.ParseFiles("views/register.html")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("message : " + err.Error()))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	tmpl.Execute(w, nil)
+}
+
+func register(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	name := r.PostForm.Get("name")
+	email := r.PostForm.Get("email")
+
+	password := r.PostForm.Get("password")
+	passwordHash, _ := bcrypt.GenerateFromPassword([]byte(password), 10)
+
+	_, err = connection.Conn.Exec(context.Background(), "INSERT INTO tb_user(name, email, password) VALUES ($1, $2, $3)", name, email, passwordHash)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("message : " + err.Error()))
+		return
+	}
+
+	var store = sessions.NewCookieStore([]byte("SESSIONS_ID"))
+	session, _ := store.Get(r, "SESSIONS_ID")
+
+	session.AddFlash("successfully registered!", "message")
+
+	session.Save(r, w)
+
+	http.Redirect(w, r, "/login", http.StatusMovedPermanently)
+}
+
+func formLogin(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	var tmpl, err = template.ParseFiles("views/login.html")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("message : " + err.Error()))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	tmpl.Execute(w, nil)
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	var store = sessions.NewCookieStore([]byte("SESSIONS_ID"))
+	session, _ := store.Get(r, "SESSIONS_ID")
+
+	err := r.ParseForm()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	email := r.PostForm.Get("email")
+	password := r.PostForm.Get("password")
+
+	user := User{}
+
+	err = connection.Conn.QueryRow(context.Background(), "SELECT * FROM tb_user WHERE email=$1", email).Scan(
+		&user.Id, &user.Name, &user.Email, &user.Password,
+	)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("message : " + err.Error()))
+		return
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("message : " + err.Error()))
+		return
+	}
+
+	session.Values["IsLogin"] = true
+	session.Values["Name"] = user.Name
+	session.Options.MaxAge = 10800 // 3 hours
+
+	session.AddFlash("successfully login!", "message")
+	session.Save(r, w)
+
+	http.Redirect(w, r, "/", http.StatusMovedPermanently)
+}
+
+func logout(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Logout")
+
+	var store = sessions.NewCookieStore([]byte("SESSIONS_ID"))
+	session, _ := store.Get(r, "SESSIONS_ID")
+	session.Options.MaxAge = -1
+
+	session.Save(r, w)
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
